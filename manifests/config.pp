5c95971040f9091dc58c3c759c0522e937b6e4a1
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2017-11-25 08:44:51 +0100

    Create config.pp

---
+++ manifests/config.pp
@@ -0,0 +1,30 @@
+# == Class: role_oradb::config
+#
+class role_oradb::config {
+
+  host { $::fqdn:
+    ensure       => 'present',
+    host_aliases => [$::hostname, 'localhost'],
+    ip           => $::ipaddress,
+    target       => '/etc/hosts',
+  }
+  
+  $all_groups = ['oinstall','dba' ,'oper']
+
+  group { $all_groups :
+    ensure      => present,
+  }
+
+  user { 'oracle' :
+    ensure      => present,
+    uid         => 500,
+    gid         => 'oinstall',
+    groups      => ['oinstall','dba','oper'],
+    shell       => '/bin/bash',
+    password    => '$1$DSJ51vh6$4XzzwyIOk6Bi/54kglGk3.',
+    home        => "/home/oracle",
+    comment     => "This user oracle was created by Puppet",
+    require     => Group[$all_groups],
+    managehome  => true,
+  }
+}
