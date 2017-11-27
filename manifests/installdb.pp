a1376ee63c7cd03f46394769abdd596d9165f803
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2017-11-27 13:31:58 +0100

    Create link to libclntsh.so.11.1
    # java.lang.UnsatisfiedLinkError: /oracle/product/11.2/db/lib/libnjni11.so: libclntsh.so.11.1: cannot open shared object file: No such file or directory

--- manifests/installdb.pp
+++ manifests/installdb.pp
@@ -4,4 +4,13 @@ class role_oradb::installdb(
   $installdb_hash,
   ) {
     create_resources('oradb::installdb', $installdb_hash)
+   
+  # java.lang.UnsatisfiedLinkError: /oracle/product/11.2/db/lib/libnjni11.so: libclntsh.so.11.1: cannot open shared object file: No such file or directory
+  file { '/oracle/product/11.2/db/lib/libclntsh.so.11.1':
+    ensure => link,
+    target => '/oracle/product/11.2/db/inventory/Scripts/ext/lib/libclntsh.so.11.1',
+    owner => 'oracle',
+    group => 'oinstall'
+  }
+
 }
